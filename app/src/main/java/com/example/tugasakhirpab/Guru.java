a22bc26fc5e0d1b9c56e86cf75398ea864d9835c
Rizqi Edining <rizqiwaskito@gmail.com>
2021-08-06 17:02:59 +0700

    first commit

---
+++ app/src/main/java/com/example/tugasakhirpab/Guru.java
@@ -0,0 +1,21 @@
+package com.example.tugasakhirpab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Guru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guru);
+
+        // ini adalah guru yang telah dibina
+        // falih asu
+
+        // dadkwkdadkdkaakdak
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tugasakhirpab/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tugasakhirpab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
